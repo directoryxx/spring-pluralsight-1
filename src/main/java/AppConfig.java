c62f7d610582087b30b89c1b71c4b9a1bdd8628d
directoryx <anggawijaya4567@gmail.com>
2020-06-08 12:23:41 +0700

    implment bean scope singleton

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,14 +1,17 @@
 import com.directoryxx.repository.SpeakerRepository;
 import com.directoryxx.repository.SpeakerRepositoryImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.directoryxx.service.SpeakerService;
 import com.directoryxx.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService(){
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
