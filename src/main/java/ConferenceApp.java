30b9ddfb6ffea83c772a43a65545cbf8b002f3aa
directoryx <anggawijaya4567@gmail.com>
2020-06-08 13:03:34 +0700

    load configuration xml

--- src/main/java/ConferenceApp.java
+++ src/main/java/ConferenceApp.java
@@ -3,13 +3,15 @@ import com.directoryxx.service.SpeakerService;
 import com.directoryxx.service.SpeakerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ConferenceApp {
 
     public static void main(String args[]){
 
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        SpeakerService service = new SpeakerServiceImpl(new SpeakerRepositoryImpl());
+        SpeakerService service = appContext.getBean("speakerService",SpeakerService.class);
 
 
         System.out.println(service.findAll().get(0).getFirstName());
--- src/main/java/com/directoryxx/service/SpeakerServiceImpl.java
+++ src/main/java/com/directoryxx/service/SpeakerServiceImpl.java
@@ -3,8 +3,6 @@ package com.directoryxx.service;
 import com.directoryxx.model.Speaker;
 import com.directoryxx.repository.SpeakerRepository;
 import com.directoryxx.repository.SpeakerRepositoryImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,18 +11,12 @@ public class SpeakerServiceImpl implements SpeakerService{
     private SpeakerRepository repository ;
 
 
-
-    public SpeakerServiceImpl (SpeakerRepository speakerRepository){
-        System.out.println("With repository constructor");
-        repository = speakerRepository;
-    }
-
     public List<Speaker> findAll(){
         return repository.findAll();
     }
 
-    public void setRepository(SpeakerRepository repository) {
-        System.out.println("SpeakServiceImpl setter");
+    public void setSpeakerRepository(SpeakerRepository repository) {
         this.repository = repository;
     }
+
 }
