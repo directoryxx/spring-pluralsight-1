d13ca0a3dcbbb29cf001cf6d681f76ad0f1c59a7
directoryx <anggawijaya4567@gmail.com>
2020-06-08 12:33:31 +0700

    auto wired demo

--- src/main/java/com/directoryxx/service/SpeakerServiceImpl.java
+++ src/main/java/com/directoryxx/service/SpeakerServiceImpl.java
@@ -3,6 +3,7 @@ package com.directoryxx.service;
 import com.directoryxx.model.Speaker;
 import com.directoryxx.repository.SpeakerRepository;
 import com.directoryxx.repository.SpeakerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -11,7 +12,12 @@ public class SpeakerServiceImpl implements SpeakerService{
 
     private SpeakerRepository repository ;
 
+    public SpeakerServiceImpl(){
+        System.out.println("No args constructor");
+    }
+
     public SpeakerServiceImpl (SpeakerRepository speakerRepository){
+        System.out.println("With repository constructor");
         repository = speakerRepository;
     }
 
@@ -19,7 +25,9 @@ public class SpeakerServiceImpl implements SpeakerService{
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakServiceImpl setter");
         this.repository = repository;
     }
 }
