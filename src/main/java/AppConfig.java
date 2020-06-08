bc99bf65dd5435d082bce290ffcff589f883f08b
directoryx <anggawijaya4567@gmail.com>
2020-06-08 17:21:39 +0700

    add util

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,5 +1,6 @@
 import com.directoryxx.repository.SpeakerRepository;
 import com.directoryxx.repository.SpeakerRepositoryImpl;
+import com.directoryxx.util.CalendarFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +9,23 @@ import com.directoryxx.service.SpeakerService;
 import com.directoryxx.service.SpeakerServiceImpl;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"com.directoryxx"})
 public class AppConfig {
 
+    @Bean(name = "calendarFac")
+    public CalendarFactory calendarFactory(){
+        CalendarFactory calendarFactory = new CalendarFactory();
+        calendarFactory.addDays(2);
+        return calendarFactory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception{
+        return calendarFactory().getObject();
+    }
     /*
     @Bean(name = "speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)
--- src/main/java/com/directoryxx/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/directoryxx/repository/SpeakerRepositoryImpl.java
@@ -1,14 +1,18 @@
 package com.directoryxx.repository;
 
 import com.directoryxx.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class SpeakerRepositoryImpl implements SpeakerRepository {
 
+    @Autowired
+    private Calendar calendarFac;
 
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<Speaker>();
@@ -17,6 +21,8 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Angga");
         speaker.setLastName("Wijaya");
+
+        System.out.println("Calendar" + calendarFac.getTime());
 
         speakers.add(speaker);
 
